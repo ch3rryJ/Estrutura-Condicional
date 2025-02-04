1bf07c45dc42e123647d58ea6d6ce5aef0455e2a
Júlia Pereira <jutella31@gmail.com>
2025-02-04 09:14:17 -0300

    Initial commit

---
+++ if.js
@@ -0,0 +1,70 @@
+console.log("atv1")
+let idade = prompt("Qual sua idade?")
+
+if (idade >= 18) {
+    console.log("Você é maior de idade")
+} else {
+    console.log("Você é menor de idade")
+}
+
+//-----------------------------------------
+console.log("atv2")
+let numero = prompt("Escolha um número:")
+
+let resultado = numero % 2
+if (resultado == 0) {
+    console.log("Par")
+} else {
+    console.log("Impar")
+}
+
+//-----------------------------------------
+console.log("atv3")
+let nota = prompt("Qual a sua nota:")
+
+if (nota >= 7){
+    console.log("Aprovado")
+} else {
+    console.log("Reprovado")
+}
+
+//-----------------------------------------
+console.log("atv4")
+let idade2 = prompt("Entrada permitida apenas para maiores de idade. Qual é a sua idade?")
+
+if (idade2 >= 18) {
+    console.log("Entrada permitida")
+} else {
+    console.log("entrada proibida")
+}
+
+//-----------------------------------------
+console.log("atv5")
+let senha = prompt("Insira a senha:")
+
+if (senha == 1234){
+    console.log("Senha correta")
+} else {
+    console.log("Senha incorreta")
+}
+
+//-----------------------------------------
+console.log("atv6")
+let temperatura = prompt("Insira a temperatura:")
+
+if (temperatura > 30){
+    console.log("Esta quente")
+} else {
+    console.log("Está frio")
+}
+//-----------------------------------------
+console.log("atv7")
+
+let numero2 = prompt("Escolha um número:")
+
+let resultado2 = numero % 2
+if (resultado2 == 0) {
+    console.log("Número positivo")
+} else {
+    console.log("Número negativo")
+}
